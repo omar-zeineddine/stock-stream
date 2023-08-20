af292e2c5ec504e6c59bbe04450bf3dab7b463e0
Omar Zeineddine <ozeineddine.dev@gmail.com>
2023-08-20 19:05:47 +0300

    style - pathname conditional rendering

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import HeadingSection from "../components/HeadingSection";
 
 const About: React.FC = () => {
-  return <div>About: React.FC</div>;
+  return (
+    <main>
+      <HeadingSection
+        title="About Stock News"
+        subtitle="latest stock news headlines from around the web."
+      />
+    </main>
+  );
 };
 
 export default About;
--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -1,7 +1,16 @@
+"use client";
 import React from "react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const Sidebar: React.FC = () => {
+  const pathname = usePathname();
+  console.log(pathname);
+
+  const isActive = (path: string) => {
+    return pathname === path;
+  };
+
   return (
     <>
       <aside className="flex flex-col w-64 px-4 py-8 overflow-y-auto bg-white border-r rtl:border-r-0 rtl:border-l dark:bg-gray-900 dark:border-gray-700 flex-shrink-0">
@@ -17,7 +26,11 @@ const Sidebar: React.FC = () => {
         <div className="flex flex-col justify-between flex-1 mt-6">
           <nav>
             <Link href="/" legacyBehavior>
-              <a className="flex items-center px-4 py-2 text-gray-700 bg-gray-100 rounded-md dark:bg-gray-800 dark:text-gray-200">
+              <a
+                className={`flex items-center px-4 py-2 mt-5  transition-colors duration-300 transform rounded-md dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700  ${
+                  isActive("/") ? "dark:bg-gray-800 " : ""
+                }`}
+              >
                 <svg
                   className="w-5 h-5"
                   viewBox="0 0 24 24"
@@ -37,7 +50,11 @@ const Sidebar: React.FC = () => {
               </a>
             </Link>
             <Link href="/news" legacyBehavior>
-              <a className="flex items-center px-4 py-2 mt-5 text-gray-600 transition-colors duration-300 transform rounded-md dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700">
+              <a
+                className={`flex items-center px-4 py-2 mt-5  transition-colors duration-300 transform rounded-md dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700  ${
+                  isActive("/news") ? "dark:bg-gray-800 " : ""
+                }`}
+              >
                 <svg
                   className="w-5 h-5"
                   viewBox="0 0 24 24"
@@ -56,7 +73,11 @@ const Sidebar: React.FC = () => {
               </a>
             </Link>
             <Link href="/about" legacyBehavior>
-              <a className="flex items-center px-4 py-2 mt-5 text-gray-600 transition-colors duration-300 transform rounded-md dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700">
+              <a
+                className={`flex items-center px-4 py-2 mt-5  transition-colors duration-300 transform rounded-md dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700  ${
+                  isActive("/about") ? "dark:bg-gray-800 " : ""
+                }`}
+              >
                 <svg
                   className="w-5 h-5"
                   viewBox="0 0 24 24"
