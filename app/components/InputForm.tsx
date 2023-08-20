a52690df9d30b8209e7466a30f0c6bbdeb98b5e4
Omar Zeineddine <ozeineddine.dev@gmail.com>
2023-08-20 18:29:28 +0300

    fix - loop over input fields only

--- app/components/InputForm.tsx
+++ app/components/InputForm.tsx
@@ -27,59 +27,58 @@ const InputForm: React.FC = () => {
           <FieldArray name="stockList">
             {({ remove, push }) => (
               <>
-                {values.stockList.map((_, index) => (
-                  <div
-                    key={index}
-                    className="w-1/2 grid gap-4 mb-6 md:grid-cols-2 p-4 mx-auto rounded-lg dark:bg-gray-900 dark:border-gray-700"
-                  >
-                    <div>
-                      <label
-                        htmlFor={`stock-${index}`}
-                        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                      >
-                        Stock
-                      </label>
-                      <Field
-                        type="text"
-                        id={`stock-${index}`}
-                        name={`stockList[${index}].stock`}
-                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        placeholder="Symbol"
-                        required
-                      />
-                    </div>
-                    <div className="">
-                      <label
-                        htmlFor={`weight-${index}`}
-                        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                      >
-                        Weight
-                      </label>
-                      <div className="flex">
+                <div className="w-1/2 grid gap-4 mb-6 md:grid-cols-2 p-4 mx-auto rounded-lg dark:bg-gray-900 dark:border-gray-700">
+                  {values.stockList.map((_, index) => (
+                    <>
+                      <div key={index}>
+                        <label
+                          htmlFor={`stock-${index}`}
+                          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                        >
+                          Stock
+                        </label>
                         <Field
                           type="text"
-                          id={`weight-${index}`}
-                          name={`stockList[${index}].weight`}
+                          id={`stock-${index}`}
+                          name={`stockList[${index}].stock`}
                           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                          placeholder="%"
+                          placeholder="Symbol"
                           required
                         />
-
-                        {values.stockList.length > 1 && (
-                          <div className="flex items-center justify-center mt-2">
-                            <button
-                              type="button"
-                              className="ml-2 px-3 py-1 text-gray-700 bg-gray-200 rounded-full hover:bg-gray-300 focus:ring-2 focus:ring-blue-300 focus:outline-none dark:bg-gray-600 dark:text-white dark:hover:bg-gray-700 dark:focus:ring-blue-800"
-                              onClick={() => remove(index)}
-                            >
-                              X
-                            </button>
-                          </div>
-                        )}
                       </div>
-                    </div>
-                  </div>
-                ))}
+                      <div className="">
+                        <label
+                          htmlFor={`weight-${index}`}
+                          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                        >
+                          Weight
+                        </label>
+                        <div className="flex">
+                          <Field
+                            type="text"
+                            id={`weight-${index}`}
+                            name={`stockList[${index}].weight`}
+                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            placeholder="%"
+                            required
+                          />
+
+                          {values.stockList.length > 1 && (
+                            <div className="flex items-center justify-center mt-2">
+                              <button
+                                type="button"
+                                className="ml-2 px-3 py-1 text-gray-700 bg-gray-200 rounded-full hover:bg-gray-300 focus:ring-2 focus:ring-blue-300 focus:outline-none dark:bg-gray-600 dark:text-white dark:hover:bg-gray-700 dark:focus:ring-blue-800"
+                                onClick={() => remove(index)}
+                              >
+                                X
+                              </button>
+                            </div>
+                          )}
+                        </div>
+                      </div>
+                    </>
+                  ))}
+                </div>
 
                 <div className="flex justify-center items-center gap-4">
                   {values.stockList.length < 5 && (
