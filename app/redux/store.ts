917ceddda038fb0a10b89fff9081f4d9e9b80bb6
Omar Zeineddine <ozeineddine.dev@gmail.com>
2023-08-20 14:26:45 +0300

    feat - add news api

--- app/redux/store.ts
+++ app/redux/store.ts
@@ -1,8 +1,16 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, } from "@reduxjs/toolkit";
+
+import { stocksNewsApi } from "./features/StockNewsApi";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    [stocksNewsApi.reducerPath]: stocksNewsApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware()
+      .concat(stocksNewsApi.middleware),
 });
 
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
+
+// export type RootState = ReturnType<typeof store.getState>;
+// export type AppDispatch = typeof store.dispatch;
