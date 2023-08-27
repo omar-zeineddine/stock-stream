76265722a5898ae1441dedf2e02d361c0223e0da
Omar Zeineddine <ozeineddine.dev@gmail.com>
2023-08-27 23:38:22 +0300

    feat - calculate portfolio performance

--- app/components/InputForm.tsx
+++ app/components/InputForm.tsx
@@ -1,25 +1,81 @@
 import React from "react";
 import { Formik, Field, FieldArray, Form } from "formik";
+import axios from "axios";
+import { DateRange, Investment } from "../stock.interface";
+import { getDates, singularStockData } from "../utils";
 
-type Stock = {
-  stock: string;
-  weight: string;
+const totalInvestment: number = 1000000; // 10,000 USD in cents
+
+const getStockHistory = async (
+  investment: Investment[],
+  amount: number
+): Promise<[number, number][]> => {
+  const dateRange: DateRange = getDates();
+
+  const allStockDataResp = await Promise.all(
+    investment.map((inv) => {
+      return singularStockData(
+        inv.name,
+        dateRange,
+        (amount * inv.weight) / 100
+      );
+    })
+  );
+
+  // console.log({ allStockDataResp });
+
+  // Length of the results
+  const l: number = allStockDataResp[0].results.length;
+
+  // chart data
+  const currentStockValue: [number, number][] = [];
+
+  // Extract the value for the current month
+  const extractCurrentValueForMonth = (allData: any, m: number): number => {
+    let currentValue = 0;
+    for (let i = 0; i <= allData.length - 1; i++) {
+      currentValue += allData[i].results[m].currentValue;
+    }
+    return Number(currentValue.toFixed(2));
+  };
+
+  // Runs a loop that goes through each stock and extract the individual values for each month and adds it up
+  for (let i = 0; i < l; i++) {
+    // [timestamp, sumOfALLStockValeForMonth]
+    currentStockValue.push([
+      allStockDataResp[0].results[i].timestamp,
+      extractCurrentValueForMonth(allStockDataResp, i),
+    ]);
+  }
+
+  // An array of array => [timestamp, sumOfALLStockValeForMonth][]
+  return currentStockValue;
 };
 
-const InputForm: React.FC = () => {
-  const initialValues: Stock[] = [{ stock: "", weight: "" }];
+type Props = {
+  setChartData: React.Dispatch<React.SetStateAction<[number, number][]>>;
+};
 
-  const handleAddStock = (values: Stock[]) => {
+const InputForm: React.FC<Props> = (props: Props) => {
+  const initialValues: Investment[] = [{ name: "", weight: 0 }];
+  const [chartData, setChartData] = React.useState<[number, number][]>([]);
+
+  /* const handleAddStock = (values: Investment[]) => {
     if (values.length < 5) {
-      values.push({ stock: "", weight: "" });
+      values.push({ stock: "", weight: 0 });
     }
+  }; */
+
+  const generateStockHistory = async (values: Investment[]) => {
+    const res = await getStockHistory(values, totalInvestment);
+    setChartData(res);
   };
 
   return (
     <Formik
       initialValues={{ stockList: initialValues }}
       onSubmit={(values) => {
-        console.log(values.stockList);
+        generateStockHistory(values.stockList);
       }}
     >
       {({ values }) => (
@@ -40,7 +96,7 @@ const InputForm: React.FC = () => {
                         <Field
                           type="text"
                           id={`stock-${index}`}
-                          name={`stockList[${index}].stock`}
+                          name={`stockList[${index}].name`}
                           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                           placeholder="Symbol"
                           required
@@ -85,7 +141,7 @@ const InputForm: React.FC = () => {
                     <button
                       type="button"
                       className="ml-2 px-3 py-1 text-gray-700 bg-gray-200 rounded-full hover:bg-gray-300 focus:ring-2 focus:ring-blue-300 focus:outline-none dark:bg-gray-600 dark:text-white dark:hover:bg-gray-700 dark:focus:ring-blue-800"
-                      onClick={() => push({ stock: "", weight: "" })}
+                      onClick={() => push({ name: "", weight: 0 })}
                     >
                       +
                     </button>
