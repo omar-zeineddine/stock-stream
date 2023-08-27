3ba9c3dd49937374be5d2aeeff46d14cf79299a4
Omar Zeineddine <ozeineddine.dev@gmail.com>
2023-08-27 23:46:54 +0300

    style - update button styles

--- app/components/InputForm.tsx
+++ app/components/InputForm.tsx
@@ -115,7 +115,7 @@ const InputForm: React.FC<Props> = (props: Props) => {
                             type="text"
                             id={`weight-${index}`}
                             name={`stockList[${index}].weight`}
-                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-500 dark:focus:border-gray-500"
                             placeholder="%"
                             required
                           />
@@ -124,7 +124,7 @@ const InputForm: React.FC<Props> = (props: Props) => {
                             <div className="flex items-center justify-center mt-2">
                               <button
                                 type="button"
-                                className="ml-2 px-3 py-1 text-gray-700 bg-gray-200 rounded-full hover:bg-gray-300 focus:ring-2 focus:ring-blue-300 focus:outline-none dark:bg-gray-600 dark:text-white dark:hover:bg-gray-700 dark:focus:ring-blue-800"
+                                className="ml-2 px-3 py-1 text-gray-700 bg-gray-200 rounded-full hover:bg-gray-300 focus:ring-2 focus:ring-blue-300 focus:outline-none dark:bg-gray-600 dark:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-800"
                                 onClick={() => remove(index)}
                               >
                                 X
@@ -141,7 +141,7 @@ const InputForm: React.FC<Props> = (props: Props) => {
                   {values.stockList.length < 5 && (
                     <button
                       type="button"
-                      className="ml-2 px-3 py-1 text-gray-700 bg-gray-200 rounded-full hover:bg-gray-300 focus:ring-2 focus:ring-blue-300 focus:outline-none dark:bg-gray-600 dark:text-white dark:hover:bg-gray-700 dark:focus:ring-blue-800"
+                      className="ml-2 px-3 py-1 text-gray-700 bg-gray-200 rounded-full hover:bg-gray-300 focus:ring-2 focus:ring-blue-300 focus:outline-none dark:bg-gray-600 dark:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-800"
                       onClick={() => push({ name: "", weight: 0 })}
                     >
                       +
@@ -150,9 +150,9 @@ const InputForm: React.FC<Props> = (props: Props) => {
 
                   <button
                     type="submit"
-                    className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                    className="text-white bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
                   >
-                    Submit
+                    Analyze
                   </button>
                 </div>
               </>
