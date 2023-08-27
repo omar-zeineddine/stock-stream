62bae1b560281498fe61eaa9acf052c9eee1c4a2
Omar Zeineddine <ozeineddine.dev@gmail.com>
2023-08-27 23:17:27 +0300

    feat - add interfaces definitions files

---
+++ app/stock.interface.ts
@@ -0,0 +1,37 @@
+export interface Investment {
+  name: string;
+  weight: number;
+}
+
+export interface StockData {
+  o: number;
+  c: number;
+  h: number;
+  l: number;
+  t: number;
+  ticker: string;
+  weight: number;
+  amount: number;
+  noOfStock: number;
+  currentValue: number;
+}
+
+export interface ApiResponse {
+  adjusted: boolean;
+  results: StockData[];
+  status: string;
+  ticker: string;
+}
+
+export interface SingleMonthData {
+  currentValue: number;
+  timestamp: number;
+}
+
+export interface SingleStockData {
+  ticker: string;
+  amount: number;
+  results: SingleMonthData[];
+}
+
+export type DateRange = [string, string];
